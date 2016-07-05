0064f82e51436da491bd6ed28f70d72c6dff5288
yisak <yisak.abdella@gmail.com>
2016-07-05 23:39:24 +0200

    Started with a class which can facilitate data extraction.

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -1,63 +1,133 @@
-import sys
-sys.path.insert(0,'D:/users/ysa/shyft_main/shyft')
-from os import path
 from datetime import datetime
+import numpy as np
 
-from shyft.orchestration.configuration.yaml_configs import YAMLSimConfig
-from shyft.orchestration.simulators.config_simulator import ConfigSimulator
 
-# For testing from statkraft repos (GIS service & SMG)
-config_dir = "D:/users/ysa/config/config_test"
-config_file = path.join(config_dir, "simulation.yaml")
-config_section = "neanidelva"
+class DataExtraction(object):
+    def __init__(self, simulator):
+        self.sim = simulator
+        self.r_m = self.sim.region_model
+        self.r_env = self.sim.region_env
+        self.cells = self.r_m.cells
+        self.nb_cells = self.r_m.size()  # len(self.cells)
+        self.geo_attr_names = ['x', 'y', 'z', 'area', 'catchment_idx', 'radiation_slope_factor',
+                               'glacier_fraction', 'lake_fraction', 'reservoir_fraction', 'forest_fraction',
+                               'unknown_fraction']
+        self.geo_attr = ['x', 'y', 'z', 'area', 'c_idx', 'rsf', 'gf', 'lf', 'rf', 'ff', 'uf']
+        self.c_id_map = self.r_m.catchment_id_map
 
-print('\nConfiguring simulation for region {}'.format(config_section))
-cfg = YAMLSimConfig(config_file, config_section)
-simulator = ConfigSimulator(cfg)
-print('Done initializing...')
-simulator.run()
-print('Done simulating...')
+    def get_input_source(self, src_type):
+        src_vct = getattr(self.r_env, src_type)
+        src_ts = [src.ts for src in src_vct]
+        src_len = [ts.size() for ts in src_ts]
+        v = np.array([ts.values.to_numpy() for ts in src_ts])
+        t = np.array([[datetime.utcfromtimestamp(ts.time(i)) for i in range(l)] for ts, l in zip(src_ts, src_len)])
 
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 1 - Input climate sources (before interpolation)
+# ----------------------------------------------------------------------------------------------------------------------
+
 precip_src_vct = simulator.region_env.precipitation
-nb_precip_src = len(precip_src_vct)
-p_idx = 0
-p_src = precip_src_vct[p_idx]
-p_src_x = p_src.mid_point().x
-p_src_y = p_src.mid_point().y
-p_src_z = p_src.mid_point().z
-p_src_ts = p_src.ts
-p_src_v = p_src_ts.values.to_numpy()
-p_src_t = [datetime.utcfromtimestamp(p_src_ts.time(i)) for i in range(len(p_src_ts))]
+temp_src_vct = simulator.region_env.temperature
+ws_src_vct = simulator.region_env.wind_speed
+rh_src_vct = simulator.region_env.rel_hum
+rad_src_vct = simulator.region_env.radiation
 
-# Cell geo data
+src_vct = {src_type: getattr(simulator.region_env, src_type)
+           for src_type in ['precipitation', 'temperature', 'wind_speed', 'rel_hum', 'radiation']}
+precip_src_vct = src_vct['precipitation']
 
-# 2 - Interpolated climate inputs
+# ----------------------------------------------------------------------------------------------------------------------
+# 2 - Cell geo data
+# ----------------------------------------------------------------------------------------------------------------------
 
-# 2.1 - Aggregated over catchments
+# ----------------------------------------------------------------------------------------------------------------------
+# 3 - Interpolated climate inputs
+# ----------------------------------------------------------------------------------------------------------------------
 
-# 2.2 - Non-aggregated (at cell level)
-
-# 2.2.1 - Timeseries (for all timsteps) at specific cell
-
-# 2.2.2 - Raster (for all cells) at a specific timestep
-
-# 3 - Responses
-
+# ----------------------------------------------------------------------------------------------------------------------
 # 3.1 - Aggregated over catchments
+# ----------------------------------------------------------------------------------------------------------------------
 
+p_agg_extractor = simulator.region_model.statistics.precipitation
+temp_agg_extractor = simulator.region_model.statistics.temperature
+rad_agg_extractor = simulator.region_model.statistics.radiation
+rh_agg_extractor = simulator.region_model.statistics.rel_hum
+ws_agg_extractor = simulator.region_model.statistics.wind_speed
+
+agg_var_extractor = {var_type: getattr(simulator.region_model.statistics, var_type)
+           for var_type in ['precipitation', 'temperature', 'wind_speed', 'rel_hum', 'radiation']}
+p_agg_extractor = agg_var_extractor['precipitation']
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 3.2 - Non-aggregated (at cell level)
+# ----------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------------------------------
 # 3.2.1 - Timeseries (for all timsteps) at specific cell
+# ----------------------------------------------------------------------------------------------------------------------
 
+def p_cell_extractor(idx): return cells[idx].env_ts.precipitation
+def temp_cell_extractor(idx): return cells[idx].env_ts.temperature
+def rad_cell_extractor(idx): return cells[idx].env_ts.radiation
+def rh_cell_extractor(idx): return cells[idx].env_ts.rel_hum
+def ws_cell_extractor(idx): return cells[idx].env_ts.wind_speed
+
+cell_env_ts_extractor = {src_type: lambda idx: getattr(cells[idx].env_ts, src_type)
+                         for src_type in ['precipitation', 'temperature', 'wind_speed', 'rel_hum', 'radiation']}
+p_cell_extractor = cell_env_ts_extractor['precipitation']
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 3.2.2 - Raster (for all cells) at a specific timestep
+# ----------------------------------------------------------------------------------------------------------------------
 
-# 4 - States
 
+# ----------------------------------------------------------------------------------------------------------------------
+# 4 - Responses
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
 # 4.1 - Aggregated over catchments
+# ----------------------------------------------------------------------------------------------------------------------
 
+p_agg_extractor = simulator.region_model.statistics.precipitation
+temp_agg_extractor = simulator.region_model.statistics.temperature
+rad_agg_extractor = simulator.region_model.statistics.radiation
+rh_agg_extractor = simulator.region_model.statistics.rel_hum
+ws_agg_extractor = simulator.region_model.statistics.wind_speed
+q_agg_extractor = simulator.region_model.statistics.discharge
+
+agg_var_extractor = {var_type: getattr(simulator.region_model.statistics, var_type)
+           for var_type in ['precipitation', 'temperature', 'wind_speed', 'rel_hum', 'radiation', 'discharge']}
+p_agg_extractor = agg_var_extractor['precipitation']
+# ----------------------------------------------------------------------------------------------------------------------
 # 4.2 - Non-aggregated (at cell level)
+# ----------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------------------------------
 # 4.2.1 - Timeseries (for all timsteps) at specific cell
+# ----------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------------------------------------------------------------
 # 4.2.2 - Raster (for all cells) at a specific timestep
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5 - States
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5.1 - Aggregated over catchments
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5.2 - Non-aggregated (at cell level)
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5.2.1 - Timeseries (for all timsteps) at specific cell
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5.2.2 - Raster (for all cells) at a specific timestep
+# ----------------------------------------------------------------------------------------------------------------------
